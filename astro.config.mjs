358db560d81a4ab04493a110a85327a54681070e
aychernov <sashacool75@gmail.com>
2025-05-29 16:33:16 +0300

    add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://aychernov.github.io",
   base: "de",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
